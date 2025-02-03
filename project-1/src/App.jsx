ab00cb0f8a1faf8c081e5114f68fbb0e8e595816
upendra9798 <upendrakushwaha022@gmail.com>
2025-02-03 16:17:24 +0530

    Project-1 (Basics)

---
+++ project-1/src/App.jsx
@@ -0,0 +1,14 @@
+const App = () => {   // Component-(name start with capital letter)
+  return (
+    <div>
+     <nav>
+      {/* in js classname is used in place of class in html */}
+      <div className="logo">
+        
+        </div>   
+     </nav>
+  </div>
+  );
+};
+
+export default App;
