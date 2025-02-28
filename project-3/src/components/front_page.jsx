81cdc1d11689a747552d542fa61840458139d968
upendra9798 <upendrakushwaha022@gmail.com>
2025-03-01 02:22:10 +0530

    Dice game

---
+++ project-3/src/components/front_page.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+
+const Front_page = () =>{
+    return (
+        <section className="container">
+            <div>
+            <img src="/images/dices.png" alt="dice" />
+            </div>
+
+            <div className='front_content'>
+                <p>DICE GAME</p>
+                <button>Play Now</button>
+            </div>
+        </section>
+    )
+}
+
+export default Front_page
